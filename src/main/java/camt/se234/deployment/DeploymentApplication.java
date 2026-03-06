65e92427f55e5c43617baf84fb6b99a2e17dac83
Jord <90235483+Jord018@users.noreply.github.com>
2026-03-06 09:15:44 +0700

    Add comment in DeploymentApplication.java
    Added a comment in the DeploymentApplication class.

--- src/main/java/camt/se234/deployment/DeploymentApplication.java
+++ src/main/java/camt/se234/deployment/DeploymentApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+//COMMENT
 @SpringBootApplication
 public class DeploymentApplication {
 
